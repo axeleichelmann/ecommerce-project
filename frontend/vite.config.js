f1c2f8a15eae48575e084f2ad0d765a255aea420
Axel Eichelmann <eichelmannaxel@gmail.com>
2025-02-05 11:34:04 +0000

    Updated allowed hosts for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Allow external access (needed for Docker & Cloud Run)
+    port: 5173, // Ensure Vite runs on the correct port
+    allowedHosts: [
+      "web-app-frontend-50293729231.europe-west10.run.app" // Allow Cloud Run domain
+    ]
+  }
 })
