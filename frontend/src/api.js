30a75f6d42e27446e9be39046d852cff3e9d9195
Axel Eichelmann <eichelmannaxel@gmail.com>
2025-02-17 09:39:57 +0000

    Fixing production URL not in allowed hosts bug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
-import { API_URL } from '../vite.config';
+
+// Import staging or production environment variable
+const environment = import.meta.env.STAGING_OR_PROD;
+
+// Define appropriate environment API url
+const API_URL =
+  environment === "PROD"
+    ? "https://ecommerce-api-production-50293729231.europe-west10.run.app"
+    : "https://product-recs-api-50293729231.europe-west10.run.app";
+
+console.log(`Using API URL: ${API_URL}`);
+
+
 
 // Create an instance of axios with the base url
 const api = axios.create({
     baseURL : API_URL
 });
+
 
 // Export api instance
 export default api;
