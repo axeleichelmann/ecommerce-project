84c0f0e6cb740d14b0fb76e355e7a016246938a1
Axel Eichelmann <eichelmannaxel@gmail.com>
2025-02-17 10:18:16 +0000

    Fixing production URL not in allowed hosts bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Allow external access (needed for Docker & Cloud Run)
-    port: 5173, // Ensure Vite runs on the correct port
-    allowedHosts: [  // Allow Cloud Run Frontend domain
-      "web-app-frontend-production-50293729231.europe-west10.run.app",
-      "web-app-frontend-50293729231.europe-west10.run.app" 
-    ]
+    port: 5173 // Ensure Vite runs on the correct port
+//    allowedHosts: [  // Allow Cloud Run Frontend domains
+//     "web-app-frontend-production-50293729231.europe-west10.run.app",
+//      "web-app-frontend-50293729231.europe-west10.run.app" 
+//    ]
   }
 });
